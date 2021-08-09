f501eda91b20ecbbf0d1009f4eaa3e65e2fdd673
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-08-09 21:07:19 +0600

    add all done

--- problem.js
+++ problem.js
@@ -1,27 +1,63 @@
-// seer to moon
-// problem-1
+
+// problem-1  seer to moon
+
 
 function seerToMoon(seer){
     let moonCalculation =  seer / 40;
+
+      if(typeof seer != 'number'){
+        return 'please give a number';
+      }
+      if(seer <= 0){
+       return 'please enter a positive number ';
+      }
     return moonCalculation;
 }
 
-const totalMoon = seerToMoon(120);
+const totalMoon = seerToMoon(20);
 console.log(totalMoon);
+
 
 // problem-2 total sales calculation
 
+
 function totalSales(shirt,pant,shoes){
+
     const  shirtPrice =100;
     const pantPrice = 200;
     const shoesPrice = 500;
-    let salesShirt = shirt * shirtPrice;
-    let salesPant = pant * pantPrice ;
-    let salesShoes = shoes * shoesPrice;
+
+      if(typeof shirt != 'number'){
+        return 'please give shirt number';
+      }
+      if(typeof pant != 'number'){
+        return 'please give pant  number';
+      }
+      if(typeof shoes != 'number'){
+        return 'please give shoes number';
+      }
+
+      if(shirt <=-1){
+        return 'please enter positiv  shart sales number ';
+       }
+
+       if(pant <=-1){
+        return 'please enter positiv  pant sales number ';
+       }
+       if(shoes <=-1){
+        return 'please enter positiv shoes sales number ';
+       }
+
+      
+    const salesShirt = shirt * shirtPrice;
+    const salesPant = pant * pantPrice ;
+    const salesShoes = shoes * shoesPrice;
     const totalPrice = salesShirt + salesPant + salesShoes;
+    
+    
     return totalPrice;
 }
-const perDaySales = totalSales(4 ,3,2);
+const perDaySales = totalSales(7, 5,0);
 console.log(perDaySales);
 
 
@@ -31,11 +67,14 @@ function deliveryCost(quantity){
     const first100DeliveryCost = 100;
     const second100DeliveryCost = 80;
     const moreThen200DeliveryCost = 50;
-    // let totalCost = 0;
+    
+     if(typeof quantity != 'number'){
+        return 'please give a number not string';
+      }
     if( quantity<=1){
         return "minimum delivery quantity 1"                                     
-
     }
+   
     else if(quantity<= 100){
       const costCount = quantity * first100DeliveryCost;
         return costCount;
@@ -57,11 +96,27 @@ function deliveryCost(quantity){
 
     }
 }
- const yourDeliveryCost = deliveryCost('-3');
+ const yourDeliveryCost = deliveryCost(450);
  console.log(yourDeliveryCost);
 
 
  //problem-4 
-  const friends =['farhad','fahim','farhan','evan','sohan'];
 
- function perfectFriend(friends)
+
+  const friends =['farhad','mukid','farhan','evan','jamil'];
+
+ function perfectFriend(friend){
+     for(let i=0; i<friends.length;i++){
+
+         let myFriend =friends[i];
+
+         if(myFriend.length == 5){
+            return myFriend;
+        }
+        
+           
+     }
+     return friend;
+ }
+ const array = perfectFriend(friends);
+ console.log(array);
